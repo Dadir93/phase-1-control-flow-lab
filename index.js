b583acc33eca3bb0fa08bd69744ddb130d2f36f7
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-01-13 02:49:33 +0300

    commit

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  let greeting;
+  if (someValue <= 400) {
+    greeting = 'This one is on me!';
+  } else if (someValue > 2000 && someValue <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (someValue > 2500) {
+    greeting = 'No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
